31b96af6855cf7636a1272a3690dffcd40ca94cc
pramaswamy <pkaran.ramaswamy@gmail.com>
2020-01-15 17:41:26 +0530

    first code changes

---
+++ src/test/java/com/pkaran/microservice/cloudnamingserver/CloudNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkaran.microservice.cloudnamingserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
